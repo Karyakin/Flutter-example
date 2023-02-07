3c7636d9c247d0201afb74529a2c6d45d766a8af
d.karyakin <3754479555552@gmail.com>
2023-02-07 13:05:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ScreenWidget());
+
+class ScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Text')),
+        ),
+        body: SimpleWidget(),
+      ),
+    );
+  }
+}
+
+class SimpleWidget extends StatefulWidget {
+  @override
+  State<SimpleWidget> createState() => _SimpleWidgetState();
+}
+
+class _SimpleWidgetState extends State<SimpleWidget> {
+  @override
+  int _count = 0;
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('$_count'),
+          ElevatedButton(
+            onPressed: (_habdleButton),
+            child: Text('PUSH'),
+            style: ElevatedButton.styleFrom(),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _habdleButton() {
+    setState(() {
+      _count++;
+    });
+  }
+}
+
+// class SimpleWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child:
+//             const Center(child: Text('Мой текст', textDirection: TextDirection.ltr)));
+//   }
+// }
