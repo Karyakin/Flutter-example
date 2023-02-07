54c5f2887681ae6103b9d520cd826b7e175944cf
d.karyakin <3754479555552@gmail.com>
2023-02-07 15:55:45 +0300

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(ScreenWidget());
@@ -10,62 +12,69 @@ class ScreenWidget extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text('ContainerWidget')),
         ),
-        body: ContainerWidget(),
+        body: (SimpleWidget()),
       ),
     );
   }
 }
 
-class ContainerWidget extends StatefulWidget {
-  const ContainerWidget({super.key});
+class TextWidget extends StatelessWidget {
+  const TextWidget(
+      {super.key,
+      required this.text,
+      required this.color,
+      required this.wids,
+      required this.fontsize});
 
-  @override
-  State<ContainerWidget> createState() => _ContainerWidgetState();
-}
+  final String text;
+  final Color color;
+  final double wids;
+  final double fontsize;
 
-class _ContainerWidgetState extends State<ContainerWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      //color: Colors.green,
+    return Container(
+      height: 100,
+      width: wids,
+      color: color,
       child: Text(
-        'Panda',
+        text,
         style: TextStyle(
-            fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),
+          fontSize: fontsize,
+        ),
       ),
-      height: 200,
-      width:
-          double.infinity, //занимает 100% по отношению к родительскому элементу
-      alignment:
-          Alignment.center, // выравнивает дочерний элемент по центру контейнера
-      padding: EdgeInsets.only(top: 5, left: 5), // внутренние отступы
-      margin: EdgeInsets.all(50), // внешний отступ
-      //transform: Matrix4.rotationZ(0.1),//поворачиваем контейнер относительно осей
-      decoration: BoxDecoration(
-          //gradient: LinearGradient(colors: [Colors.red, Colors.green, Colors.yellow])) ,//фоновое оформление контейнера. Если указывать тут то нужно удалить цвет контейнера указанный выше
-          color: Colors.green,
-          image: DecorationImage(
-              image: Image.network(
-                      'https://poltavawave.com.ua/media/poltavska-khvilia_xcwi/AaZR_5RnR.jpeg')
-                  .image,
-              fit: BoxFit.cover),
-          // borderRadius: BorderRadius.circular(15),
-          // border: Border.all(
-          //   color: Colors.black,// цвет границы контейнера
-          //   width: 5
-          // )
-          shape: BoxShape.circle,
-          // ignore: prefer_const_literals_to_create_immutables
-          boxShadow: [
-            const BoxShadow(
-                color: Colors.black45, // цвет тени
-                offset: Offset(-5, 5), //смещение тени от рамки
-                blurRadius: 5, //размытие
-                spreadRadius:
-                    5 //величина на которую должен быть увеличен контейнер перед применнением размытия
-                )
-          ]),
-    ));
+    );
   }
 }
+
+class SimpleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment:
+          MainAxisAlignment.spaceEvenly, //расположение дочерних элементов
+      mainAxisSize:
+          MainAxisSize.min, //сколько места должно быть занято в главной оси
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      //для параметра CrossAxisAlignment.baseline необходимо указывать параметр textBaseLine : ideographic и alphabetic https://www.w3schools.com/tags/img_textbaseline.gif, //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline
+      textBaseline: TextBaseline.alphabetic,
+      textDirection:
+          TextDirection.rtl, // расположение текста справа на лево и наоборот
+      verticalDirection: VerticalDirection
+          .down, //расположение элементов вниз/вверх. Сработает только для колонки. Контролирует направление дочерних элементов
+
+      // ignore: prefer_const_literals_to_create_immutables
+      children: <Widget>[
+        TextWidget(
+            color: Colors.limeAccent, wids: 120, text: 'First', fontsize: 50),
+        TextWidget(
+            color: Colors.cyanAccent, wids: 180, text: 'Second', fontsize: 40),
+        TextWidget(
+            color: Colors.purpleAccent, wids: 100, text: 'Thir', fontsize: 30)
+      ],
+    );
+  }
+}
+
+
+// next https://www.youtube.com/watch?v=I5DF3CFDG8s&list=PL6lh8cTntlDiLlH_rHl5F0JOy_gRm_Wa4&index=7&ab_channel=ProFlutter%D1%83%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D0%B0%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC
