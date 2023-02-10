7836c1eaee88a4f8e4924defcb38b192cdcbe4b9
d.karyakin <3754479555552@gmail.com>
2023-02-10 10:17:12 +0300

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ScreenWidget());
@@ -10,26 +11,23 @@ class ScreenWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('ListView Widget')),
+          title: Center(child: Text('GridView Widget')),
         ),
-        body: (ListViewSeparatedWidget()),
+        body: (GridViewCount()),
       ),
     );
   }
 }
 
-class SimpleWidget extends StatelessWidget {
+//элементы создаются лениво, т.е. по мере прокрутки подгружаются, а более старые уничтожаюься
+class GridViewWidget extends StatelessWidget {
+  const GridViewWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection:
-          Axis.vertical, // направление списка по горизонтали или вертикали
-      reverse: false, // переворачивает список снизу в верх
-      controller: ScrollController(
-          initialScrollOffset: 100), // управление начальным смещение прокрутки
-      physics:
-          ScrollPhysics(), // определяет, как будет вести себя список, когда пользователь достигнет конца, или когда пользователь прекращает прокрутку
-      padding: EdgeInsets.all(20),
+    return GridView(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       // ignore: prefer_const_literals_to_create_immutables
       children: [
         TextWidget(text: '1'),
@@ -50,34 +48,41 @@ class SimpleWidget extends StatelessWidget {
   }
 }
 
-//элементы списка создаются лениво. т.е. по мере прокрутки, а те, что уже ушли вверх, более ранние элементы(прокручены - удаляются). Хорошо длч больштх списков
-class ListViewBuilderWidget extends StatelessWidget {
+class GridViewWUnlim extends StatelessWidget {
+  const GridViewWUnlim({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        // itemCount: 30, // это свойство можно закоментировать и список станет бесконечным, т.к. элементы подгружаюься лениво, т.е. по мере зпроса
+    return GridView.builder(
+        //itemCount: 20,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, index) {
-      return TextWidget(text: '$index');
-    });
+          return TextWidget(text: '$index');
+        });
   }
 }
 
-//список с разделителем между каждым элементом. Фактически два переплетенных списка: один основной, второй переплетенный с основным список разделителей
-// разделителем может быть любой виджет(текст, картинка или другой любой виджет)
-class ListViewSeparatedWidget extends StatelessWidget {
+class GridViewCount extends StatelessWidget {
+  const GridViewCount({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        return TextWidget(text: '$index');
-      },
-      separatorBuilder: (context, index) {
-        return Divider(
-          color: Colors.red,
-          thickness: 2,
-        );
-      },
-      itemCount: 20,
+    return GridView.count(
+      crossAxisCount: 2,
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        TextWidget(text: '1'),
+        TextWidget(text: '2'),
+        TextWidget(text: '3'),
+        TextWidget(text: '4'),
+        TextWidget(text: '5'),
+        TextWidget(text: '6'),
+        TextWidget(text: '7'),
+        TextWidget(text: '8'),
+        TextWidget(text: '9'),
+        TextWidget(text: '10'),
+      ],
     );
   }
 }
@@ -106,4 +111,4 @@ class TextWidget extends StatelessWidget {
   }
 }
 
-// !url https://www.youtube.com/watch?v=I5DF3CFDG8s&list=PL6lh8cTntlDiLlH_rHl5F0JOy_gRm_Wa4&index=7&ab_channel=ProFlutter%D1%83%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D0%B0%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC
+// !https://www.youtube.com/watch?v=ZudufqYrpco&list=PL6lh8cTntlDiLlH_rHl5F0JOy_gRm_Wa4&index=9&ab_channel=ProFlutter%D1%83%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D0%B0%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC
