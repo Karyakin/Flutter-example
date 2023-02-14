beecf1500b015a02528d059649bf2b6569af7090
d.karyakin <3754479555552@gmail.com>
2023-02-14 18:07:30 +0300

    fin

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,16 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/Example.dart';
 import 'package:untitled/widgets/description.dart';
+import 'package:untitled/widgets/navigators/AnonymousRoutes.dart';
 import 'package:untitled/widgets/positionedWidget.dart';
 import 'package:untitled/widgets/stackwidget.dart';
 
 import 'widgets/IndexedStack.dart';
 
-void main() => runApp(ScreenWidget());
-
-class ScreenWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Stack & Positioned Widgets'),
-        ),
-         //body: Example(),
-         // body: StackWidget(),
-        //body: PositionedWidget(),
-        body: IndexedStackWidget(),
-      ),
-    );
-  }
+void main() {
+  runApp(AnonymousRoutes());
 }
 
-// https://www.youtube.com/watch?v=VR1D-HQDi_o&list=PL6lh8cTntlDiLlH_rHl5F0JOy_gRm_Wa4&index=14&ab_channel=ProFlutter%D1%83%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D0%B0%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC
+
+
+// https://www.youtube.com/watch?v=m5QQzfkvqA0&list=PL6lh8cTntlDiLlH_rHl5F0JOy_gRm_Wa4&index=16&ab_channel=ProFlutter%D1%83%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D0%B0%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC
---
+++ lib/widgets/navigators/AnonymousRoutes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnonymousRoutes extends StatelessWidget {
+  const AnonymousRoutes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
